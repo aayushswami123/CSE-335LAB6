47e6fefbeb80c70fddc889a75902df6f3612d44a
Aayush Swami <aa>
2025-04-01 16:30:00 -0700

    Initial Commit

---
+++ Lab6Tests/Lab6Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Lab6Tests.swift
+//  Lab6Tests
+//
+//  Created by Apple on 4/1/25.
+//
+
+import Testing
+
+struct Lab6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
